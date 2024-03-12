76d94b411e5b46d12e670dd8a3544f19d4cc7a59
Rob England <robenng@icloud.com>
2024-03-12 14:59:05 -0300

    Adjust dimensions button works

--- scripts/grid.js
+++ scripts/grid.js
@@ -1,17 +1,25 @@
-const gridDiv = document.getElementById('grid');
-let dimensions = 4;
+createGrid(4);
+document.getElementById("adj-dimensions").addEventListener('click', () => {
+  const newDimension = prompt('Please enter new dimension (max 100)');
+  createGrid(newDimension);
+});
 
-for(let i = 0; i < dimensions ; i++) {
-  const rowDiv = document.createElement('div');
-  rowDiv.classList.add('grid-row');
-  rowDiv.style.height = `${100/dimensions}%`;
+function createGrid(dimensions) {
+  const gridDiv = document.getElementById("grid");
+  gridDiv.innerHTML = '';
 
-  for(let j = 0; j < dimensions; j++) {
-    const itemDiv = document.createElement('div');
-    itemDiv.classList.add('grid-item');
-    itemDiv.style.width = `${100/dimensions}%`;
-    rowDiv.appendChild(itemDiv);
+  for (let i = 0; i < dimensions; i++) {
+    const rowDiv = document.createElement("div");
+    rowDiv.classList.add("grid-row");
+    rowDiv.style.height = `${100 / dimensions}%`;
+
+    for (let j = 0; j < dimensions; j++) {
+      const itemDiv = document.createElement("div");
+      itemDiv.classList.add("grid-item");
+      itemDiv.style.width = `${100 / dimensions}%`;
+      rowDiv.appendChild(itemDiv);
+    }
+
+    gridDiv.appendChild(rowDiv);
   }
-
-  gridDiv.appendChild(rowDiv);
 }
